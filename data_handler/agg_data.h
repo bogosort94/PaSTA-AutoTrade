e8e3f39dbc83dcc7fff7869a27ea1a7851ee25c2
Boyu Tian <bytian@umich.edu>
2021-03-16 20:59:46 -0700

    AggDataStore
    The data structure to store aggregate data for all stocks for a specific
    window size.

--- data_handler/agg_data.h
+++ data_handler/agg_data.h
@@ -1,8 +1,11 @@
 #ifndef AGG_DATA_H_
 #define AGG_DATA_H_
 
+#include "absl/container/flat_hash_map.h"
+#include "absl/flags/flag.h"
 #include "proto/data.pb.h"
 
+#include <deque>
 #include <string>
 
 namespace pasta {
@@ -16,20 +19,62 @@ class AggregateData {
 
   void UpdateWith(const AggregateDataProto& agg_data);
 
- private:
   std::string ticker_;
-  int vol_;
-  int acc_vol_;
+  int64_t vol_;
+  int64_t acc_vol_;
   double day_open_;
   double vwap_;
   double open_;
   double close_;
   double high_;
   double low_;
-  int start_;
-  int end_;
+  int64_t start_;
+  int64_t end_;
+};
+
+class AggDataStore {
+ public:
+  AggDataStore();
+  AggDataStore(int64_t window_size);
+
+  typedef std::deque<AggregateData> AggDataQueue;
+  const AggDataQueue& GetData(const std::string& ticker);
+
+  // Add new aggregate data to the data store.
+  // Returns true if an aggregate window is closed.
+  // Note that when this method returns true, there can be at most two closing
+  // aggregate window (previous and new). However, if a new window is closed
+  // right upon creation, it does not make the return value true when the next
+  // aggregate window is created.
+  bool AddData(const AggregateDataProto& data_proto);
+
+  void Clear();
+
+ private:
+  // Add new aggregate data to a specific queue.
+  bool AddDataHelper(const AggregateDataProto& data_proto, AggDataQueue* data);
+
+  // Determine if the new data still belongs to the previous aggregate window.
+  bool IsNewAggregate(const AggregateDataProto& data_proto,
+                      const AggregateData& prev_agg) const;
+
+  // Given the start ts of a piece of data, calculate the start ts of the
+  // aggregate window it belongs to.
+  int64_t AggWindowStart(int64_t start) const;
+
+  // Returns true if an aggregate window at the head of queue is closed.
+  bool IsClosed(const AggregateData& data) const;
+
+  // The size of the aggregate window (seconds).
+  int64_t window_size_;
+
+  // The queue storing aggregate data. New data will be added to head of queue.
+  // Old data will be popped from back of queue.
+  absl::flat_hash_map<std::string, AggDataQueue> data_;
 };
 
 }  // namespace pasta
+
+extern absl::Flag<int64_t> FLAGS_agg_data_store_size;
 
 #endif  // AGG_DATA_H_
--- data_handler/agg_data_test.cc
+++ data_handler/agg_data_test.cc
@@ -6,11 +6,29 @@
 #include "gtest/gtest.h"
 #include "proto/data.pb.h"
 
+#include <boost/format.hpp>
+
 namespace pasta {
 
 namespace {
 
+std::string MakeAggProto(std::string ev, std::string sym, int64_t v, int64_t av,
+                         double op, double vw, double o, double c, double h,
+                         double l, double a, int64_t z, int64_t s, int64_t e) {
+  auto fmt = boost::format(
+                 R"tag({"ev":"%1%","sym":"%2%","v":%3%,"av":%4%,"op":%5%,)tag"
+                 R"tag("vw":%6%,"o":%7%,"c":%8%,"h":%9%,"l":%10%,"a":%11%,)tag"
+                 R"tag("z":%12%,"s":%13%,"e":%14%})tag") %
+             ev % sym % v % av % op % vw % o % c % h % l % a % z % s % e;
+  return fmt.str();
+}
+
 class AggregateDataTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    one_sec.Clear();
+    ten_sec.Clear();
+  }
  protected:
   AggregateDataProto GetAggDataProtoFromString(
       const std::string& agg_data_str) {
@@ -20,94 +38,31 @@ class AggregateDataTest : public ::testing::Test {
     CHECK(s.ok());
     return proto;
   }
+
+  AggDataStore one_sec = AggDataStore(1);
+  AggDataStore ten_sec = AggDataStore(10);
 };
 
 std::string kTestCases_1[] = {
-    R"tag({
-   "ev": "A",
-   "sym": "SPCE",
-   "v": 200,
-   "av": 8642007,
-   "op": 25.66,
-   "vw": 25.3981,
-   "o": 25.39,
-   "c": 25.45,
-   "h": 25.57,
-   "l": 25.35,
-   "a": 25.4014,
-   "z": 50,
-   "s": 1610144868000,
-   "e": 1610144869000
-  })tag",
-    R"tag({
-   "ev": "A",
-   "sym": "SPCE",
-   "v": 100,
-   "av": 8642107,
-   "op": 25.66,
-   "vw": 25.3980,
-   "o": 25.43,
-   "c": 25.47,
-   "h": 25.49,
-   "l": 25.30,
-   "a": 25.4714,
-   "z": 50,
-   "s": 1610144869000,
-   "e": 1610144870000
-  })tag",
-    R"tag({
-   "ev": "A",
-   "sym": "SPCE",
-   "v": 600,
-   "av": 8642707,
-   "op": 25.66,
-   "vw": 25.3983,
-   "o": 25.45,
-   "c": 25.54,
-   "h": 25.61,
-   "l": 25.44,
-   "a": 25.5714,
-   "z": 50,
-   "s": 1610144870000,
-   "e": 1610144871000
-  })tag",
+    MakeAggProto("A", "SPCE", 200, 8642007, 25.66, 25.3981, 25.39, 25.45, 25.57,
+                 25.35, 25.4014, 50, 1610144868000, 1610144869000),
+    MakeAggProto("A", "SPCE", 100, 8642107, 25.66, 25.3980, 25.43, 25.47, 25.49,
+                 25.30, 25.4714, 50, 1610144869000, 1610144870000),
+    MakeAggProto("A", "SPCE", 600, 8642707, 25.66, 25.3983, 25.45, 25.54, 25.61,
+                 25.44, 25.5714, 50, 1610144870000, 1610144871000),
 };
 
 std::string kResult_1 =
-    R"tag({
-   "ev": "A",
-   "sym": "SPCE",
-   "v": 900,
-   "av": 8642707,
-   "op": 25.66,
-   "vw": 25.3983,
-   "o": 25.39,
-   "c": 25.54,
-   "h": 25.61,
-   "l": 25.30,
-   "a": 25.5714,
-   "z": 50,
-   "s": 1610144868000,
-   "e": 1610144871000
-  })tag";
+    MakeAggProto("A", "SPCE", 900, 8642707, 25.66, 25.3983, 25.39, 25.54, 25.61,
+                 25.30, 25.5714, 50, 1610144868000, 1610144871000);
 
-std::string kTestCase_2 =
-    R"tag({
-   "ev": "A",
-   "sym": "AAPL",
-   "v": 100,
-   "av": 8642107,
-   "op": 25.66,
-   "vw": 25.3980,
-   "o": 25.43,
-   "c": 25.47,
-   "h": 25.49,
-   "l": 25.30,
-   "a": 25.4714,
-   "z": 50,
-   "s": 1610144869000,
-   "e": 1610144870000
-  })tag";
+std::string kTestCase_2 = 
+    MakeAggProto("A", "AAPL", 100, 8642107, 25.66, 25.3980, 25.43, 25.47, 25.49,
+                 25.30, 25.4714, 50, 1610144869000, 1610144870000);
+
+std::string kTestCase_3 = 
+    MakeAggProto("A", "SPCE", 900, 8642707, 25.66, 25.3983, 25.39, 25.54, 25.61,
+                 25.30, 25.5714, 50, 1610144880000, 1610144881000);
 
 TEST_F(AggregateDataTest, UpdateAgg) {
   AggregateData agg(GetAggDataProtoFromString(kTestCases_1[0]));
@@ -120,6 +75,25 @@ TEST_F(AggregateDataTest, UpdateAgg) {
 TEST_F(AggregateDataTest, UpdateWrongTicker) {
   AggregateData agg(GetAggDataProtoFromString(kTestCases_1[0]));
   ASSERT_DEATH(agg.UpdateWith(GetAggDataProtoFromString(kTestCase_2)), "");
+}
+
+TEST_F(AggregateDataTest, OneSecAlwaysClose) {
+  for (int i = 0; i < 3; ++i) {
+    EXPECT_TRUE(one_sec.AddData(GetAggDataProtoFromString(kTestCases_1[i])));
+  }
+  EXPECT_TRUE(one_sec.AddData(GetAggDataProtoFromString(kTestCase_3)));
+  EXPECT_EQ(one_sec.GetData("SPCE").size(), 4);
+  EXPECT_TRUE(one_sec.GetData("AAPL").empty());
+}
+
+TEST_F(AggregateDataTest, MultiSecCloseAndAlignment) {
+  EXPECT_FALSE(ten_sec.AddData(GetAggDataProtoFromString(kTestCases_1[0])));
+  EXPECT_TRUE(ten_sec.AddData(GetAggDataProtoFromString(kTestCases_1[1])));
+  EXPECT_FALSE(ten_sec.AddData(GetAggDataProtoFromString(kTestCases_1[2])));
+  auto data = ten_sec.GetData("SPCE");
+  EXPECT_EQ(data.size(), 2);
+  EXPECT_EQ(data[0].start_, 1610144870000);
+  EXPECT_EQ(data[1].start_, 1610144860000);
 }
 
 }  // namespace
